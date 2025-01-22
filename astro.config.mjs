904818ae92c7279cff9db5abd0d6af9291e29870
Steven Burrows <60275700+stevenjburrows@users.noreply.github.com>
2025-01-22 16:47:57 +0000

    updating the template with some changes I made to my final blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg.netlify.app/",
+	site: "https://kilted-coder-blog.netlify.app/",
 	base: "/",
 	integrations: [sitemap()],
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,30 +1,21 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getAllPosts } from "../lib/client";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 
-let posts = await getCollection("posts");
-
-posts = posts.sort(
-	(a, b) =>
-		new Date(b.data.updated || b.data.added).valueOf() -
-		new Date(a.data.updated || a.data.added).valueOf()
-);
-
+const data = await getAllPosts();
+const allPosts = data.publication.posts.edges;
 export const GET = () =>
 	rss({
 		title: SITE_TITLE || "",
 		description: SITE_DESCRIPTION || "",
 		site: import.meta.env.SITE,
-		items: posts.map((post) => {
+		items: allPosts.map((post) => {
 			return {
-				link: `/post/${post.data.slug}`,
-				title: post.data.title,
-				pubDate: post.data.added,
-				description: post.data.description,
-				content: post.rendered.html,
-				customData: `<updated>${
-					post.data.updated ? post.data.updated : ""
-				}</updated>`,
+				link: `/post/${post.node.slug}`,
+				title: post.node.title,
+				pubDate: post.node.publishedAt,
+				description: post.node.brief,
+				content: post.node.content.html,
 			};
 		}),
 		stylesheet: "/rss-styles.xsl",
